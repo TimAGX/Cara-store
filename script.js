ddeb4266f7ad5d7721cee9204b7a3f2686944008
johnmadakin <edafeadjeke@gmail.com>
2022-07-18 11:50:22 +0100

    added JS funtionalities

--- script.js
+++ script.js
@@ -12,23 +12,6 @@ close.addEventListener('click', ()=>{
   nav.classList.remove('active')
 })
 
-// // const mainImg = document.getElementById('mainimg');
-// // const smallImg = document.getElementsByClassName('small-img');
-
-// // smallImg[0].onclick = function(){
-// //   mainImg.src = smallImg[0].src;
-// // }
-// // smallImg[1].onclick = function(){
-// //   mainImg.src = smallImg[1].src;
-// // }
-// // smallImg[2].onclick = function(){
-// //   mainImg.src = smallImg[2].src;
-// // }
-// // smallImg[3].onclick = function(){
-// //   mainImg.src = smallImg[3].src;
-// // }
-
-
 //prodSess.append(items());
 //fetch funtion
 const prodSess = document.getElementById('flex')

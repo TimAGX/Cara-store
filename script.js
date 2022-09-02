732d1a09cc073443d2788cd437d1a422612bf7e2
johnmadakin <edafeadjeke@gmail.com>
2022-09-02 16:37:28 +0100

    paystack

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const close = document.getElementById('close');
 
 let shirtInfo
 
-
 bar.addEventListener('click', ()=>{
   nav.classList.toggle('active')
 });
@@ -68,6 +67,7 @@ const prodSess = document.getElementById('flex')
 
       data2.items2.map(shirts2 =>{
         prodSess2.append(items2(shirts2))
+        console.log(shirts2)
       });
     });
   });
@@ -147,10 +147,12 @@ function updateQuantity(value, id) {
 
 
 }
+
+let total;
 function cart(){
   cartTotal()
   console.log(data);
-  const cartTable = document.getElementById('tablee');
+  let cartTable = document.getElementById('tablee');
 let cartTab = "";
   data.map((carts, id) =>{
     cartTab +=`
@@ -172,11 +174,44 @@ function cartTotal() {
   let totatDom =document.getElementById("total");
   let subTotalDom = document.getElementById("subTotal")
 
-  let total = parseInt(data.reduce((acc , current) => acc + (current.price * current.quantity), 0))
+  total = parseInt(data.reduce((acc , current) => acc + (current.price * current.quantity), 0))
   console.log(total);
-
   totatDom.innerHTML = "#"+total
   subTotalDom.innerHTML = "#"+total
+}
 
+function payWithPaystack() {
 
+    const handler = PaystackPop.setup({
+        key: 'pk_test_c962b44121cfb161a74ba8b67294cc83734e4768', //put your public key here
+        email: 'ayomidetimothy28@gmail.com', //put your customer's email here
+        amount: total * 100, //amount the customer is supposed to pay
+        metadata: {
+            custom_fields: [
+                {
+                    display_name: "308670846",
+                    variable_name: "mobile_number",
+                    value: "+2348012345678" //customer's mobile number
+                }
+            ]
+        },
+        callback: function (response) {
+            //after the transaction have been completed
+            //make post call  to the server with to verify payment
+            //using transaction reference as post data
+            $.post("verify.php", {reference:response.reference}, function(status){
+                if(status == "success")
+                    //successful transaction
+                    alert('Transaction was successful');
+                else
+                    //transaction failed
+                    alert(response);
+            });
+        },
+        onClose: function () {
+            //when the user close the payment modal
+            alert('Transaction cancelled');
+        }
+    });
+    return handler.openIframe(); //open the paystack's payment modal
 }
